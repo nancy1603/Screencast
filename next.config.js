00784a57cd62ff57712c8f362e77fb7bf7f59792
Nancy <agarwalnancy1603@gmail.com>
2022-01-11 15:28:02 +0530

    api added

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withImages = require('next-images')
 module.exports = withImages()
 module.exports = {
     env: {
-      api: "http://20.124.101.228/",
+      api: "https://screencast.weblikate.com",
     },
   };
 
